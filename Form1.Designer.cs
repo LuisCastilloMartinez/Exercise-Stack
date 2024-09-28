f88281a4ac16f25929dc8e2babd2197aa72763fb
Luis Castillo <apointer252@gmail.com>
2024-09-27 19:12:15 -0600

    Stack Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Pilas_Práctica_No1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.lbPila = new System.Windows.Forms.ListBox();
+            this.lblTopElement = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(21, 25);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(62, 27);
+            this.txtElemento.StateCommon.Border.Color1 = System.Drawing.Color.SteelBlue;
+            this.txtElemento.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.txtElemento.StateCommon.Border.Rounding = 6;
+            this.txtElemento.TabIndex = 0;
+            // 
+            // btnPush
+            // 
+            this.btnPush.BackColor = System.Drawing.Color.White;
+            this.btnPush.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnPush.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPush.Font = new System.Drawing.Font("Nunito", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPush.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnPush.Location = new System.Drawing.Point(227, 22);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 30);
+            this.btnPush.TabIndex = 1;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = false;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.BackColor = System.Drawing.Color.White;
+            this.btnPop.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnPop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPop.Font = new System.Drawing.Font("Nunito", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPop.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnPop.Location = new System.Drawing.Point(227, 58);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 30);
+            this.btnPop.TabIndex = 2;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = false;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.BackColor = System.Drawing.Color.White;
+            this.btnPeek.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnPeek.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPeek.Font = new System.Drawing.Font("Nunito", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeek.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnPeek.Location = new System.Drawing.Point(227, 94);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(75, 30);
+            this.btnPeek.TabIndex = 3;
+            this.btnPeek.Text = "Peek";
+            this.btnPeek.UseVisualStyleBackColor = false;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // lbPila
+            // 
+            this.lbPila.FormattingEnabled = true;
+            this.lbPila.Location = new System.Drawing.Point(21, 65);
+            this.lbPila.Name = "lbPila";
+            this.lbPila.Size = new System.Drawing.Size(191, 95);
+            this.lbPila.TabIndex = 4;
+            // 
+            // lblTopElement
+            // 
+            this.lblTopElement.AutoSize = true;
+            this.lblTopElement.Font = new System.Drawing.Font("Nunito", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopElement.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTopElement.Location = new System.Drawing.Point(25, 166);
+            this.lblTopElement.Name = "lblTopElement";
+            this.lblTopElement.Size = new System.Drawing.Size(0, 18);
+            this.lblTopElement.TabIndex = 5;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.White;
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Nunito", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnSalir.Location = new System.Drawing.Point(227, 130);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 30);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(330, 203);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblTopElement);
+            this.Controls.Add(this.lbPila);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.txtElemento);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Rounding = 6;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtElemento;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.ListBox lbPila;
+        private System.Windows.Forms.Label lblTopElement;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace Pilas_Práctica_No1
+{
+    public partial class Form1 : KryptonForm
+    {
+        private Stack<String> Pila = new Stack<String>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            string elemento = txtElemento.Text;
+            if(!string.IsNullOrEmpty(elemento))
+            {
+                Pila.Push(elemento);
+                lbPila.Items.Insert(0, elemento);
+                txtElemento.Clear();
+                txtElemento.Focus();
+                lblTopElement.Text = $"Elemento en el tope: {Pila.Peek()}";
+            }
+            else { MessageBox.Show("Por favor, ingrese un elemento para agregar a la pila."); }
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if (Pila.Count > 0) {
+                string eliminado = Pila.Pop();
+                lbPila.Items.RemoveAt(0);
+                if (Pila.Count > 0)  lblTopElement.Text = $"Elemento en el tope: {Pila.Peek()}";
+                else lblTopElement.Text = "La pila está vacía.";
+            }
+            else { MessageBox.Show("La pila está vacía."); }
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if (Pila.Count > 0) MessageBox.Show($"Elemento en el tope de la pila: {Pila.Peek()}", "Peek");
+            else MessageBox.Show("Pila vacia.");
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e) { Application.Exit(); }
+    }
+}
